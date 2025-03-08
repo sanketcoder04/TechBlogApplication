51c3ea397976ff0ed89a545f65ec63e8f788555a
Sanket Ganguly <sanketganguly53@gmail.com>
2025-03-08 20:20:23 +0530

    Update EditServlet.java

--- src/main/java/techblog/servlets/EditServlet.java
+++ src/main/java/techblog/servlets/EditServlet.java
@@ -1,27 +1,21 @@
 package techblog.servlets;
 
-import java.io.File;
 import java.io.IOException;
 import java.sql.SQLException;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.servlet.http.Part;
 
 import techblog.dao.UserDao;
 import techblog.entity.User;
 import techblog.helper.ConnectionProvider;
-import techblog.helper.HelperToManageFiles;
 
 @SuppressWarnings("serial")
-@MultipartConfig
 public class EditServlet extends HttpServlet{
 
-	@SuppressWarnings("deprecation")
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		//fetch all data to update
@@ -30,8 +24,6 @@ public class EditServlet extends HttpServlet{
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
 		String about = req.getParameter("about");
-		Part part = req.getPart("image");
-		String imageName = part.getSubmittedFileName();
 		
 		// Session Data Fetching
 		
@@ -42,26 +34,17 @@ public class EditServlet extends HttpServlet{
 			resp.sendRedirect("login.jsp");
 		}
 		String oldEmail = user.getEmail();
-		String oldProfile = user.getProfile();
 		
 		user.setName(name);
 		user.setEmail(email);
 		user.setPassword(password);
 		user.setAbout(about);
-		user.setProfile(imageName);
 		
 		// Update data into DataBase 
 		
 		UserDao userdb = new UserDao(ConnectionProvider.getConnection());
 		try {
 			userdb.updateUser(user, oldEmail);
-			
-			String path = req.getRealPath("/") + "profiles" + File.separator + user.getProfile();
-			String oldPath = req.getRealPath("/") + "profiles" + File.separator + oldProfile;
-			if(!oldProfile.equals("default.png")) {
-				HelperToManageFiles.deleteFile(oldPath);
-			}
-			HelperToManageFiles.saveFile(part.getInputStream(), path);
 			
 			session.setAttribute("user", user);
 			resp.sendRedirect("profile.jsp");
